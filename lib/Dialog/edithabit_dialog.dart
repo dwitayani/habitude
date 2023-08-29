1c7465c6593f217ad1a9b49d43085859eeac15b7
dwitayani <dwitayani2005@gmail.com>
2023-08-29 17:26:56 +0700

    habit_widget

---
+++ lib/Dialog/edithabit_dialog.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class EditHabitDialog extends StatefulWidget {
+  const EditHabitDialog({super.key});
+
+  @override
+  State<EditHabitDialog> createState() => _EditHabitDialogState();
+}
+
+class _EditHabitDialogState extends State<EditHabitDialog> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/widgets/habit_widget.dart
+++ lib/widgets/habit_widget.dart
@@ -22,242 +22,221 @@ class HabitWidget extends StatefulWidget {
 
 class _HabitWidgetState extends State<HabitWidget> {
   bool isOpen = false;
+  DateTime today = DateTime.now();
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: MediaQuery.of(context).size.width,
-      margin: const EdgeInsets.symmetric(
-        horizontal: 16,
-        vertical: 4,
-      ),
-      padding: const EdgeInsets.all(24.0),
-      decoration: BoxDecoration(
-        color: const Color.fromARGB(250, 255, 209, 201),
-        borderRadius: BorderRadius.circular(48.0),
-        border: Border.all(
-          color: Colors.black,
-          width: 1.0,
+        width: MediaQuery.of(context).size.width,
+        margin: const EdgeInsets.symmetric(
+          horizontal: 16,
+          vertical: 4,
         ),
-      ),
-      child: Column(
-        children: [
-          Row(
-            children: [
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      widget.judul,
-                      style: TextStyle(
-                        fontSize: 24,
-                        fontFamily: 'TitanOne',
-                      ),
-                    ),
-                    Text(
-                      "${widget.jadwalHari.length} times a week",
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontFamily: 'TitanOne',
-                        color: Colors.grey,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              if (isOpen)
-                Row(
-                  children: [
-                    Icon(
-                      Icons.edit,
-                      color: Colors.blue,
-                    ),
-                    InkWell(
-                      onTap: () async {
-                        await FirebaseFirestore.instance
-                            .collection("habits")
-                            .doc(widget.id)
-                            .delete();
-                      },
-                      child: Icon(
-                        Icons.delete,
-                        color: Colors.red,
-                      ),
-                    )
-                  ],
-                ),
-              SizedBox(
-                width: 8,
-              ),
-              InkWell(
-                onTap: () {
-                  isOpen = !isOpen;
-                  setState(() {});
-                },
-                child: Icon(
-                  isOpen
-                      ? Icons.keyboard_arrow_down
-                      : Icons.keyboard_arrow_right,
-                  size: 32,
-                ),
-              )
-            ],
+        padding: const EdgeInsets.all(24.0),
+        decoration: BoxDecoration(
+          color: widget.jadwalHari.contains(DateFormat("EEEE").format(today))
+              ? Color.fromARGB(250, 255, 209, 201)
+              : Color.fromARGB(250, 255, 209, 201).withOpacity(0.5),
+          borderRadius: BorderRadius.circular(48.0),
+          border: Border.all(
+            color: Colors.black,
+            width: 1.0,
           ),
-          if (isOpen)
-            Column(
+        ),
+        child: Column(
+          children: [
+            Row(
               children: [
-                Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        "Sunday",
-                        "Monday",
-                        "Tuesday",
-                        "Wednesday",
-                        "Thursday",
-                        "Friday",
-                        "Saturday",
-                      ].map((e) {
-                        bool isScheduled = false;
-                        if (widget.jadwalHari.contains(e)) {
-                          isScheduled = true;
-                        }
-                        return Container(
-                          margin: EdgeInsets.only(top: 8.0, right: 4.0),
-                          padding: EdgeInsets.all(5),
-                          width: 32,
-                          height: 32,
-                          decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            color: isScheduled
-                                ? const Color.fromARGB(255, 255, 181, 181)
-                                : Colors.white,
-                            border: Border.all(
-                              color: Colors.grey,
-                              width: 0.5,
-                            ),
-                          ),
-                          child: Text(
-                            e[0],
-                            style: TextStyle(
-                              fontSize: 16,
-                              fontFamily: 'TitanOne',
-                              color: !isScheduled
-                                  ? const Color.fromARGB(255, 255, 181, 181)
-                                  : Colors.white,
-                            ),
-                            textAlign: TextAlign.center,
-                          ),
-                        );
-                      }).toList(),
-                    ),
-                  ],
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Reminder',
-                      style: TextStyle(
-                        fontSize: 12,
-                        fontFamily: 'TitilliumWeb',
-                        color: Colors.grey,
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        widget.judul,
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontFamily: 'TitanOne',
+                        ),
                       ),
-                    ),
-                    SingleChildScrollView(
-                      scrollDirection: Axis.horizontal,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children:
-                            List.generate(widget.jamPengingat.length, (index) {
-                          return Container(
-                            alignment: Alignment.center,
-                            margin: EdgeInsets.only(top: 4.0, right: 4.0),
-                            width: 80,
-                            height: 32,
-                            decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius: BorderRadius.circular(8.0),
-                              border:
-                                  Border.all(color: Colors.grey, width: 1.0),
-                            ),
-                            child: Text(
-                              widget.jamPengingat[index],
-                              style: TextStyle(fontSize: 20),
-                            ),
-                          );
-                        }),
+                      Text(
+                        "${widget.jadwalHari.length} days a week",
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontFamily: 'TitanOne',
+                          color: Colors.grey,
+                        ),
                       ),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Builder(builder: (context) {
-                          bool isToday = false;
-                          DateTime today = DateTime.now();
-                          if (today.isAfter(widget.interval.start) &&
-                              today.isBefore(widget.interval.end)) {
-                            if (widget.jadwalHari
-                                .contains(DateFormat("EEEE").format(today))) {
-                              isToday = true;
-                            }
-                          }
-                          if (!isToday) {
-                            return SizedBox();
-                          }
-                          return TextButton(
-                            onPressed: () {},
-                            style: TextButton.styleFrom(
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(1.0),
-                              ),
-                            ),
-                            child: Row(
-                              children: [
-                                Icon(Icons.close, color: Colors.red),
-                                SizedBox(width: 8),
-                                Text(
-                                  'missed',
-                                  style: TextStyle(
-                                    color: Colors.red,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          );
-                        }),
-                        Builder(builder: (context) {
-                          return TextButton(
-                            onPressed: () {},
-                            style: TextButton.styleFrom(
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(1.0),
-                              ),
-                            ),
-                            child: Row(
-                              children: [
-                                Icon(Icons.check, color: Colors.green),
-                                SizedBox(width: 8),
-                                Text(
-                                  'I do it',
-                                  style: TextStyle(
-                                    color: Colors.green,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          );
-                        })
-                      ],
-                    )
-                  ],
+                    ],
+                  ),
                 ),
+                if (isOpen)
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.edit,
+                        color: Colors.blue,
+                      ),
+                      InkWell(
+                        onTap: () async {
+                          await FirebaseFirestore.instance
+                              .collection("habits")
+                              .doc(widget.id)
+                              .delete();
+                        },
+                        child: Icon(
+                          Icons.delete,
+                          color: Colors.red,
+                        ),
+                      )
+                    ],
+                  ),
+                SizedBox(
+                  width: 8,
+                ),
+                InkWell(
+                  onTap: () {
+                    isOpen = !isOpen;
+                    setState(() {});
+                  },
+                  child: Icon(
+                    isOpen
+                        ? Icons.keyboard_arrow_down
+                        : Icons.keyboard_arrow_right,
+                    size: 32,
+                  ),
+                )
               ],
             ),
-        ],
-      ),
-    );
+            if (isOpen)
+              Column(
+                children: [
+                  Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          "Sunday",
+                          "Monday",
+                          "Tuesday",
+                          "Wednesday",
+                          "Thursday",
+                          "Friday",
+                          "Saturday",
+                        ].map((e) {
+                          bool isScheduled = false;
+                          if (widget.jadwalHari.contains(e)) {
+                            isScheduled = true;
+                          }
+                          return Container(
+                            margin: EdgeInsets.only(top: 8.0, right: 4.0),
+                            padding: EdgeInsets.all(5),
+                            width: 32,
+                            height: 32,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: isScheduled
+                                  ? const Color.fromARGB(255, 255, 181, 181)
+                                  : Colors.white,
+                              border: Border.all(
+                                color: Colors.grey,
+                                width: 0.5,
+                              ),
+                            ),
+                            child: Text(
+                              e[0],
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontFamily: 'TitanOne',
+                                color: !isScheduled
+                                    ? const Color.fromARGB(255, 255, 181, 181)
+                                    : Colors.white,
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                          );
+                        }).toList(),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Reminder',
+                        style: TextStyle(
+                          fontSize: 12,
+                          fontFamily: 'TitilliumWeb',
+                          color: Colors.grey,
+                        ),
+                      ),
+                      SingleChildScrollView(
+                        scrollDirection: Axis.horizontal,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: List.generate(widget.jamPengingat.length,
+                              (index) {
+                            return Container(
+                              alignment: Alignment.center,
+                              margin: EdgeInsets.only(top: 4.0, right: 4.0),
+                              width: 80,
+                              height: 32,
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(8.0),
+                                border:
+                                    Border.all(color: Colors.grey, width: 1.0),
+                              ),
+                              child: Text(
+                                widget.jamPengingat[index],
+                                style: TextStyle(fontSize: 20),
+                              ),
+                            );
+                          }),
+                        ),
+                      ),
+                      widget.jadwalHari
+                              .contains(DateFormat("EEEE").format(today))
+                          ? Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                TextButton(
+                                  onPressed: () {},
+                                  child: Row(
+                                    children: [
+                                      Icon(Icons.close, color: Colors.red),
+                                      SizedBox(width: 8),
+                                      Text(
+                                        'Missed',
+                                        style: TextStyle(
+                                          color: Colors.red,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                TextButton(
+                                  onPressed: () {},
+                                  child: Row(
+                                    children: [
+                                      Icon(Icons.check, color: Colors.green),
+                                      SizedBox(width: 8),
+                                      Text(
+                                        'I do it',
+                                        style: TextStyle(
+                                          color: Colors.green,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            )
+                          : SizedBox()
+                    ],
+                  ),
+                ],
+              ),
+          ],
+        ));
   }
 }
