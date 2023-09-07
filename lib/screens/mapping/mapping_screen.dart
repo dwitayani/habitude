694a09a15b814586e14ab784c9baa02ec662bc69
dwitayani <dwitayani2005@gmail.com>
2023-09-07 17:00:50 +0700

    [mapping]

--- lib/screens/mapping/mapping_screen.dart
+++ lib/screens/mapping/mapping_screen.dart
@@ -1,4 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+
+import '../../widgets/habit_widget.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 // import 'package:habitude_aplication/widgets/mapping_widget.dart';
 
 class MappingScreen extends StatelessWidget {
@@ -28,6 +33,39 @@ class MappingScreen extends StatelessWidget {
                   fontWeight: FontWeight.bold,
                   fontFamily: 'TitanOne',
                 ),
+              ),
+              StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+                stream:
+                    FirebaseFirestore.instance.collection("habits").snapshots(),
+                builder: (_, snapshot) {
+                  if (!snapshot.hasData) {
+                    return Text('loading data...');
+                  }
+                  List<DocumentSnapshot<Map<String, dynamic>>> docs =
+                      snapshot.data!.docs;
+                  return Column(
+                    children: List.generate(docs.length, (index) {
+                      return HabitWidget(
+                        id: docs[index].id,
+                        judul: docs[index].data()!['nama'],
+                        jadwalHari: List<String>.from(
+                            docs[index].data()!['hari_mengulang']),
+                        jamPengingat: List<String>.from(
+                            docs[index].data()!['jam_pengingat']),
+                        interval: DateTimeRange(
+                          start: DateTime.fromMillisecondsSinceEpoch(
+                              (docs[index].data()!['interval']['start']
+                                      as Timestamp)
+                                  .millisecondsSinceEpoch),
+                          end: DateTime.fromMillisecondsSinceEpoch((docs[index]
+                                  .data()!['interval']['end'] as Timestamp)
+                              .millisecondsSinceEpoch),
+                        ),
+                        habit: docs[index],
+                      );
+                    }),
+                  );
+                },
               ),
             ],
           ),
